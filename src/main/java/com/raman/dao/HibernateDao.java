c149a938890bb54d28806d0c4326dda22679c19c
RexRK <kraman938@gmail.com>
2023-08-29 12:34:52 +0530

    Dao implemented

---
+++ src/main/java/com/raman/dao/HibernateDao.java
@@ -0,0 +1,70 @@
+package com.raman.dao;
+
+import com.raman.entity.Product;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class HibernateDao implements ProductDao {
+    @Autowired
+    private Session session;
+
+    @Override
+    public void addProduct(Product product) throws DaoException {
+        ProductDao.super.addProduct(product);
+    }
+
+    @Override
+    public void updateProduct(Product product) throws DaoException {
+        ProductDao.super.updateProduct(product);
+    }
+
+    @Override
+    public Product getProduct(Integer productId) throws DaoException {
+        return ProductDao.super.getProduct(productId);
+    }
+
+    @Override
+    public void deleteProduct(Integer productId) throws DaoException {
+        ProductDao.super.deleteProduct(productId);
+    }
+
+    @Override
+    public List<Product> getAllProducts() throws DaoException {
+        Query<Product> qry = session.createQuery("from Product", Product.class);
+        return qry.getResultList();
+    }
+
+    @Override
+    public List<Product> getProductsByPriceRange(Double min, Double max) throws DaoException {
+        return ProductDao.super.getProductsByPriceRange(min, max);
+    }
+
+    @Override
+    public List<Product> getProductsInCategory(Integer categoryId) throws DaoException {
+        return ProductDao.super.getProductsInCategory(categoryId);
+    }
+
+    @Override
+    public List<Product> getProductsNotInStock() throws DaoException {
+        return ProductDao.super.getProductsNotInStock();
+    }
+
+    @Override
+    public List<Product> getProductOnOrder() throws DaoException {
+        return ProductDao.super.getProductOnOrder();
+    }
+
+    @Override
+    public List<Product> getDiscontinuedProducts() throws DaoException {
+        return ProductDao.super.getDiscontinuedProducts();
+    }
+
+    @Override
+    public long count() throws DaoException {
+        return ProductDao.super.count();
+    }
+}
